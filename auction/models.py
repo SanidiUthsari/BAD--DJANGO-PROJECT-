45607cacbeb20ca3afeec7423242efe3e6e7dac9
Luckseegan <119049199+Luckseegan@users.noreply.github.com>
2023-11-20 20:44:03 +0530

    modified auction and core when bidding

---
+++ auction/models.py
@@ -0,0 +1,13 @@
+# auction/models.py
+from django.db import models
+from django.conf import settings  # Import settings module
+
+class Bid(models.Model):
+    profile = models.ForeignKey('showroom.Profile', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bid_price = models.DecimalField(max_digits=15, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_winner = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Bid by {self.user.username} on {self.profile.name}"
---
+++ auction/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+# auction/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Bid
+from showroom.models import Profile
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+
+
+
+def profile_list(request):
+    profiles = Profile.objects.all()
+    return render(request, 'auction/profile_list.html', {'profiles': profiles})
+
+def profile_detail(request, profile_id):
+    profile = get_object_or_404(Profile, pk=profile_id)
+    return render(request, 'auction/profile_detail.html', {'profile': profile})
+
+@login_required(login_url='account:login')  # This decorator ensures that the user is logged in
+def place_bid(request, profile_id):
+    profile = get_object_or_404(Profile, pk=profile_id)
+
+    if request.method == 'POST':
+        bid_price = request.POST.get('bid_price')
+        # Validate bid_price and handle the bidding logic
+
+        Bid.objects.create(profile=profile, user=request.user, bid_price=bid_price)
+        # Redirect to a success page or back to the profile details page
+        return redirect('auction:profile_list')
+
+    return render(request, 'auction/place_bid.html', {'profile': profile})
