78698e4d641ced7d5b56ce90b7a06601edcd19d5
Luckseegan <119049199+Luckseegan@users.noreply.github.com>
2023-12-02 00:47:36 +0530

    messaging app included

--- messaging/urls.py
+++ messaging/urls.py
@@ -1,10 +1,12 @@
+
 # messaging/urls.py
 from django.urls import path
-from .views import inbox, send_message
+from .views import inbox, send_message, view_conversation
 
 app_name = 'messaging'
 
 urlpatterns = [
     path('inbox/', inbox, name='inbox'),
     path('send_message/<int:receiver_id>/', send_message, name='send_message'),
+    path('view_conversation/<int:receiver_id>/', view_conversation, name='view_conversation'),
 ]
